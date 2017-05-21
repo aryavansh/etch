48e7616a4fe91855ece444230d6f3f317cedad13
aryavansh <aryavansh@gmail.com>
2017-05-21 17:38:08 +0400

    Reupdating

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 $(function() {
-    for (var x = 0; x<16; x++) {
-        for (var y = 0; y<16; y++) {
+	var result = 16;
+		for (var x = 0; x<result; x++) {
+        for (var y = 0; y<result; y++) {
+            $("<div>").addClass("unit").appendTo('#container'); }}});
+
+function buttonFn() {
+	$('.unit').css("background-color", "lightgrey")
+			};
+
+function valueCheck() {
+	
+	while ((isNaN(result) || result<1 || result>100) && result !== null) {
+		var result = prompt("Please enter size of workspace:", "Please enter number between 1-100");
+	};
+
+	$('#container').empty();
+
+	var ht = (832/result)-2;
+	
+	for (var x = 0; x<result; x++) {
+        for (var y = 0; y<result; y++) {
             $("<div>").addClass("unit").appendTo('#container');
         }
-    }
-});
+  
 
-$(document).ready(function(){
-	$('.unit').hover(
+        $('.unit').css("height", ht);
+        $('.unit').css("width", ht);
+
+        $('.unit').hover(
 	function(){
-		$(this).css("background-color", "red")	}
-	
-	);
-
-	$('button').on("click", function() {
-		$('.unit').css("background-color", "lightgrey")
+		$(this).css("background-color", "#"+((1<<24)*Math.random()|0).toString(16))	
 	});
 
-/*	$('.unit').mouseleave(
-	function(){
-		$(this).css("background-color", "lightgrey")	}
-	
-	);
+    }};
 
-	*/
-	
-});
+$(document).ready(function(){
+
+	$('.unit').hover(
+	function(){
+		$(this).css("background-color", "#"+((1<<24)*Math.random()|0).toString(16))	
+	}); });
